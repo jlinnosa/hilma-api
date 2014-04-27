e41db64d311ed6b2197113c00419367eba40668f
Mikael Gueck <gumi@iki.fi>
2014-04-27 20:31:53 +0300

    JSR-310 support for Jackson.

--- src/main/java/io/mikael/api/hilma/Application.java
+++ src/main/java/io/mikael/api/hilma/Application.java
@@ -1,9 +1,12 @@
 package io.mikael.api.hilma;
 
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.cloud.config.java.ServiceScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -24,6 +27,11 @@ public class Application {
 
     @Configuration
     public static class DataRestConfiguration extends RepositoryRestMvcConfiguration {
+    }
+
+    @Bean
+    public Module javaTimeJsonModule() {
+        return new JSR310Module();
     }
 
     public static void main(String[] args) {
