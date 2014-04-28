8b07c07677272b7442189564aa6571959fe38edf
Mikael Gueck <gumi@iki.fi>
2014-04-28 22:21:01 +0300

    KnockoutJS frontend.

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,60 @@
+
+function ApplicationModel(stompClient) {
+  var self = this;
+
+  self.username = ko.observable();
+  self.noticeboard = ko.observable(new NoticeboardModel());
+  self.notifications = ko.observableArray();
+
+  self.connect = function() {
+    stompClient.connect({}, function(frame) {
+      stompClient.subscribe("/topic/hilma.*", function(message) {
+        self.noticeboard().addNotice(JSON.parse(message.body));
+      });
+      stompClient.subscribe("/user/queue/errors", function(message) {
+        self.pushNotification("Error " + message.body);
+      });
+    }, function(error) {
+      console.log("STOMP protocol error " + error);
+    });
+  }
+
+  self.loadModel = function() {
+    $.getJSON("/notices?sort=published,desc&size=100", function(data) {
+      var notices = data._embedded.notices;
+      for (var i = 0; i < notices.length; i++) {
+        self.noticeboard().addNotice(notices[i]);
+      }
+    })
+  }
+
+  self.pushNotification = function(text) {
+    self.notifications.push({notification: text});
+    if (self.notifications().length > 5) {
+      self.notifications.shift();
+    }
+  }
+}
+
+function NoticeboardModel() {
+  var self = this;
+  self.rows = ko.observableArray();
+  self.addNotice = function(notice) {
+    self.rows.push(new NoticeRow(notice));
+  };
+};
+
+function NoticeRow(data) {
+  var self = this;
+  self.id = data.id;
+  self.name = data.name;
+  self.link = data.link;
+  self.type = data.type;
+  self.published = data.published;
+  self.closes = data.closes;
+  self.cpv = data.cpv;
+  self.description = data.description;
+  self.organization = data.organization;
+  self.note = data.note;
+};
+
