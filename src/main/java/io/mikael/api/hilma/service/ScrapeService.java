fc812263b14f0612ca9a4d102c1ae13bc910c6a2
Mikael Gueck <gumi@iki.fi>
2014-04-29 23:47:52 +0300

    Schedule the scrape.

--- src/main/java/io/mikael/api/hilma/service/ScrapeService.java
+++ src/main/java/io/mikael/api/hilma/service/ScrapeService.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -38,6 +39,7 @@ public class ScrapeService {
     /**
      *
      */
+    @Scheduled(fixedRate=600000L, initialDelay=60000L)
     public void fetchNewNotices() throws IOException {
         final Document doc = Jsoup.connect(newListUrl).get();
         for (final ScrapedLink l : SiteScraper.parseNewLinks(doc)) {
