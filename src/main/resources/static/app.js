d58ce480896169f5f05215c03695aec6336ed55a
Mikael Gueck <gumi@iki.fi>
2014-05-08 00:39:19 +0300

    Dev database creation, fix date formatting for STOMP messages.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -65,6 +65,7 @@ function toMoment(input) {
       hour: input[3], minute: input[4]
     });
   } else {
-    return moment(input)
+    return moment(0).year(input.year).month(input.month).date(input.dayOfMonth)
+        .hours(input.hour).minutes(input.minute).seconds(input.second);
   }
 }
