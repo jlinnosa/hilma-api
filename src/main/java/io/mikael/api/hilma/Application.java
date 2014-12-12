74862d843fd940ba9575d357fe47f5e89cca40b9
Mikael Gueck <gumi@iki.fi>
2014-12-12 03:22:41 +0200

    Remove cloud configuration stuff, I'll readd it when it's more stable.

--- src/main/java/io/mikael/api/hilma/Application.java
+++ src/main/java/io/mikael/api/hilma/Application.java
@@ -3,16 +3,10 @@ package io.mikael.api.hilma;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
 import lombok.NoArgsConstructor;
-import org.h2.server.web.WebServlet;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.cloud.app.ApplicationInstanceInfo;
-import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -20,26 +14,9 @@ import org.springframework.web.socket.config.annotation.AbstractWebSocketMessage
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
-import javax.sql.DataSource;
-
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 @EnableScheduling
 public class Application {
-
-    @Configuration
-    @Profile("cloud")
-    public class CloudConfig extends AbstractCloudConfig {
-        @Bean
-        public ApplicationInstanceInfo applicationInfo() {
-            return cloud().getApplicationInstanceInfo();
-        }
-        @Bean
-        public DataSource dataSource() {
-            return connectionFactory().dataSource();
-        }
-    }
 
     @Configuration
     public static class DataRestConfiguration extends RepositoryRestMvcConfiguration {
@@ -68,7 +45,10 @@ public class Application {
         return new JSR310Module();
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args).registerShutdownHook();
+    public static void main(final String ... args) throws Exception {
+        new SpringApplicationBuilder(Application.class)
+                .registerShutdownHook(true)
+                .build()
+                .run(args);
     }
 }
