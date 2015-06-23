61f16e9b731e37192829f69b71167e6f23b86384
Mikael Gueck <gumi@iki.fi>
2015-06-23 18:17:17 +0300

    Refactor.

--- src/main/java/io/mikael/api/hilma/util/RunAs.java
+++ src/main/java/io/mikael/api/hilma/util/RunAs.java
@@ -1,35 +1,33 @@
 package io.mikael.api.hilma.util;
 
-import com.google.common.collect.ImmutableList;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.util.Collections;
+
 @Slf4j
 public class RunAs {
 
     @FunctionalInterface
-    public interface RunAsMethod {
-        default void run() {
-            try {
-                runWithException();
-            } catch (Exception e) {
-                // ignore
-                log.error("Run as method failed", e);
-            }
-        }
-        void runWithException() throws Exception;
+    public interface RunWithException {
+        void run() throws Exception;
     }
 
-    public static void runAsAdmin(final RunAsMethod func) {
-        final AnonymousAuthenticationToken token = new AnonymousAuthenticationToken("system", "system",
-                ImmutableList.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
-        final Authentication originalAuthentication = SecurityContextHolder.getContext().getAuthentication();
-        SecurityContextHolder.getContext().setAuthentication(token);
-        func.run();
-        SecurityContextHolder.getContext().setAuthentication(originalAuthentication);
+    public static void runAsAdmin(final RunWithException func) {
+        final Authentication admin = new AnonymousAuthenticationToken(
+                "system", "system", Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN")));
+        final Authentication original = SecurityContextHolder.getContext().getAuthentication();
+        SecurityContextHolder.getContext().setAuthentication(admin);
+        try {
+            func.run();
+        } catch (Exception e) {
+            log.error("Run as method failed", e);
+        } finally {
+            SecurityContextHolder.getContext().setAuthentication(original);
+        }
     }
 
 }
